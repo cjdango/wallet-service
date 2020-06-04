e7e156e9b4fd39189f2818075495bd7db068768c
cjdango <cjdango@gmail.com>
2020-06-04 15:44:09 +0800

    feat: created mutations
    created the following mutations:
    updateReservedBalance
    releaseReservedBalance
    updateVirtualBalance
    cancelVirtualBalance
    commitVirtualBalance

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,17 +1,21 @@
 import { Account } from './models';
 import mongoose, { mongo } from 'mongoose';
 
-const getContextObj = async (parent, context) => {
-  if (!(parent instanceof mongoose.Document)) {
-    parent = await Account.findById(parent.id);
-  }
+const getAccountObj = async (id) => {
+  const accountDoc = await Account.findById(id);
 
+  if (!accountDoc) return Account.create({});
+
+  return accountDoc;
+};
+
+const getContextObj = async (parent, context) => {
   let contextObj = parent.contexts.find((obj) => obj.name === context);
 
   if (!contextObj) {
     contextObj = { name: context, reservedBalance: 0, virtualBalance: 0 };
     parent.contexts.push(contextObj);
-    parent.save();
+    await parent.save();
   }
 
   return contextObj;
@@ -29,33 +33,7 @@ export default {
 
   Query: {
     account: async (_, { id }) => {
-      try {
-        id = mongoose.Types.ObjectId(id);
-      } catch {}
-
-      const accountObj = (
-        await Account.aggregate([
-          { $match: { _id: id } },
-          {
-            $project: {
-              _id: 0,
-              id: '$_id',
-              balance: 1,
-              contexts: {
-                $filter: {
-                  input: '$contexts',
-                  as: 'item',
-                  cond: { $gt: ['$$item.reservedBalance', 30] },
-                },
-              },
-            },
-          },
-        ])
-      )[0];
-
-      if (!accountObj) return Account.create({});
-
-      return accountObj;
+      return getAccountObj(id);
     },
     // accounts: () =>
     //   Account.find({
@@ -67,16 +45,55 @@ export default {
   Mutation: {
     updateBalance: async (_, { account, delta }) => {
       if (delta < 0) throw Error('Invalid delta');
-      const accountDoc: any = await Account.findById(account);
+      const accountDoc: any = await getAccountObj(account);
       accountDoc.balance = delta;
       accountDoc.save();
       return true;
     },
     createReservedBalance: async (_, { account, context, amount }) => {
-      const accountDoc: any = await Account.findById(account);
-      const contextObj = accountDoc.contexts.find((obj) => obj.name === context);
+      const accountDoc: any = await getAccountObj(account);
+      const contextObj = await getContextObj(accountDoc, context);
       contextObj.reservedBalance += amount;
       accountDoc.balance -= amount;
+      accountDoc.save();
+      return true;
+    },
+    updateReservedBalance: async (_, { account, context, delta }) => {
+      if (delta < 0) throw Error('Invalid delta');
+      const accountDoc: any = await getAccountObj(account);
+      const contextObj = await getContextObj(accountDoc, context);
+      contextObj.reservedBalance = delta;
+      accountDoc.save();
+      return true;
+    },
+    releaseReservedBalance: async (_, { account, context }) => {
+      const accountDoc: any = await getAccountObj(account);
+      const contextObj = await getContextObj(accountDoc, context);
+      accountDoc.balance += contextObj.reservedBalance;
+      contextObj.reservedBalance = 0;
+      accountDoc.save();
+      return true;
+    },
+    updateVirtualBalance: async (_, { account, context, delta }) => {
+      if (delta < 0) throw Error('Invalid delta');
+      const accountDoc: any = await getAccountObj(account);
+      const contextObj = await getContextObj(accountDoc, context);
+      contextObj.virtualBalance = delta;
+      accountDoc.save();
+      return true;
+    },
+    cancelVirtualBalance: async (_, { account, context }) => {
+      const accountDoc: any = await getAccountObj(account);
+      const contextObj = await getContextObj(accountDoc, context);
+      contextObj.virtualBalance = 0;
+      accountDoc.save();
+      return true;
+    },
+    commitVirtualBalance: async (_, { account, context }) => {
+      const accountDoc: any = await getAccountObj(account);
+      const contextObj = await getContextObj(accountDoc, context);
+      accountDoc.balance += contextObj.virtualBalance;
+      contextObj.virtualBalance = 0;
       accountDoc.save();
       return true;
     },
